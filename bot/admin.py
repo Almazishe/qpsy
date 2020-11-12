341b4b2eabb892d828280f79549f30ef55f2d0e2
Almazishe <mr.esenyazov@gmail.com>
2020-11-13 03:58:34 +0600

    wef

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import TelegramUser
+
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'active_psy')
