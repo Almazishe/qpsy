c9c964b66603b6af8c945a479d80c35e8dcd3a7d
Almazishe <mr.esenyazov@gmail.com>
2020-11-19 20:34:00 +0600

    fevf

--- bot/admin.py
+++ bot/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 
 from .models import TelegramUser
+from .models import ChatState
+
+admin.site.register(ChatState)
 
 
 @admin.register(TelegramUser)
--- bot/utils.py
+++ bot/utils.py
@@ -1,11 +1,20 @@
 from django.shortcuts import get_object_or_404
 from bot.models import TelegramUser
+from bot.models import ChatState
 
 
 def get_tg_user(chat_id):
     tg_user = get_object_or_404(TelegramUser, chat_id=chat_id)
     return tg_user
 
+def get_or_create_current_state(chat_id):
+    chat_state = ChatState.objects.get_or_create(
+        chat_id=chat_id,
+        defaults=[
+            state=0,
+        ]
+    )
+    return chat_state.state
 
 
 # def create_tg_user(chat_id):
