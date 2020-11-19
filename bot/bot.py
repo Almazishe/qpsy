2daee1f3c01348482f8715fe98b94e20aae5e5c4
Almazishe <mr.esenyazov@gmail.com>
2020-11-19 21:49:52 +0600

    rgevf

--- bot/bot.py
+++ bot/bot.py
@@ -1,11 +1,10 @@
+from telebot.apihelper import send_message
 from chat.models import SENT
 from chat.models import Message
 import time
 import telebot
 import logging
 from telebot import types
-from .models import TelegramUser
-from .models import ChatState
 from django.views.generic import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
@@ -14,7 +13,11 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+from .utils import get_psy
 from .utils import get_tg_user
+from .utils import send_message
+from .utils import create_tg_user
+from .utils import get_or_create_current_state
 
 
 WEBHOOK_HOST = 'qpsy.herokuapp.com'
@@ -38,17 +41,6 @@ reply_keyboard = [
     {'name': 'Горячая линия'},
 ]
 
-# keyboard = reply_keyboard
-#             key = types.ReplyKeyboardMarkup(True, False)
-#             for i in range(len(keyboard)):
-#                 but = types.KeyboardButton(keyboard[i]['name'])
-#                 key.add(but)
-
-#             bot.send_message(
-#                 message.chat.id,
-#                 text=f'Привет {tg_user.name}! =) , выбери что ты хотел бы сделать... ',
-#                 reply_markup=key
-#             )
 
 @method_decorator(csrf_exempt, name='dispatch')
 class BotView(View):
@@ -62,25 +54,104 @@ class BotView(View):
             bot.process_new_updates([update])
         return HttpResponse('post')
 
-
-
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     try:
         tg_user = get_tg_user(message.chat.id)
-        if tg_user:
+        bot.send_message(
+            message.chat.id,
+            text='У вас уже есть психолог' 
+        )
+    except Exception as e:
+        chat_state = get_or_create_current_state(message.chat.id)
+        if chat_state.state == 0:
+            chat_state.state = 1
+            chat_state.save()
+            bot.send_message(message.chat.id,  ('Привет, введи код школьного психолога.'))
+
+
+@bot.message_handler(func=lambda message: get_or_create_current_state(message.chat.id).state == 1,  content_types=['text'])
+def wait_code(message):
+    try:
+        chat_id = message.chat.id
+        psy = get_psy(message.text)
+        tg_user = create_tg_user(chat_id, psy)
+
+        chat_state = get_or_create_current_state(chat_id)
+        chat_state.state = 2
+        chat_state.save()
+
+        bot.send_message(message.chat.id,  ('Как к тебе обращаться? \nPS: Можешь ввести любое имя =) '))
+    except:
+        bot.send_message(message.chat.id,  'Психолога с таким кодом увы не существует, попробуй снова ввести код.')
+
+@bot.message_handler(func=lambda message: get_or_create_current_state(message.chat.id).state == 2, content_types=['text'])
+def wait_name(message):
+    try:
+        chat_id = message.chat.id
+        chat_state = get_or_create_current_state(chat_id)
+
+        tg_user = get_tg_user(chat_id)
+        tg_user.name = message.text
+        tg_user.save()
+
+        chat_state.state = 3
+        chat_state.save()
+
+        keyboard = reply_keyboard
+        key = types.ReplyKeyboardMarkup(True, False)
+        for i in range(len(keyboard)):
+            but = types.KeyboardButton(keyboard[i]['name'])
+            key.add(but)
+
+        bot.send_message(
+            chat_id,
+            text=f'Привет {tg_user.name}! =) , меня зовут {tg_user.active_psy.first_name} {tg_user.active_psy.last_name} Выбери что ты хотел бы сделать или же можешь просто начать печатать я обязательно отвечу тебе =) ',
+            reply_markup=key
+        )
+    except Exception as e:
+        bot.send_message(message.chat.id,  f'ERROR[wait_name]: {str(e)}')
+
+
+@bot.message_handler(func=lambda message: get_or_create_current_state(message.chat.id).state == 3, content_types=['text'])
+def chat(message):
+    chat_id = message.chat.id
+    try:
+        tg_user = get_tg_user(chat_id)
+        if message.text == reply_keyboard[1]['name']:
             bot.send_message(
                 message.chat.id,
-                text='У вас уже есть психолог' 
+                'Позвони тебе помогут\n' +
+                '8-(776)-168-87-60'
+            )
+        elif message.text == reply_keyboard[0]['name']:
+            bot.send_message(
+                message.chat.id,
+                f'Напиши что угодно я обязательно тебе помогу {tg_user.name}'
             )
         else:
-            bot.send_message(message.chat.id,
-                            ('Привет, введи код школьного психолога.'))
-    except Exception as e:
-        bot.send_message(
-                message.chat.id,
-                text='Ошибка в /start методе \n' + str(e) 
-            )
+            send_message(tg_user, message.text)
+    except:
+        bot.send_message(message.chat.id,  f'ERROR[chat]: {str(e)}')
+        
+    
+
+# @bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_ENTER_AGE.value)
+# def user_entering_age(message):
+#     # А вот тут сделаем проверку
+#     if not message.text.isdigit():
+#         # Состояние не меняем, поэтому только выводим сообщение об ошибке и ждём дальше
+#         bot.send_message(message.chat.id, "Что-то не так, попробуй ещё раз!")
+#         return
+#     # На данном этапе мы уверены, что message.text можно преобразовать в число, поэтому ничем не рискуем
+#     if int(message.text) < 5 or int(message.text) > 100:
+#         bot.send_message(message.chat.id, "Какой-то странный возраст. Не верю! Отвечай честно.")
+#         return
+#     else:
+#         # Возраст введён корректно, можно идти дальше
+#         bot.send_message(message.chat.id, "Когда-то и мне было столько лет...эх... Впрочем, не будем отвлекаться. "
+#                                           "Отправь мне какую-нибудь фотографию.")
+#         dbworker.set_state(message.chat.id, config.States.S_SEND_PIC.value)
 
 
 # @bot.message_handler(content_types=['text'])
--- bot/utils.py
+++ bot/utils.py
@@ -1,7 +1,32 @@
 from django.shortcuts import get_object_or_404
 from bot.models import TelegramUser
 from bot.models import ChatState
+from chat.models import Message
+from chat.models import SENT
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
+def send_message(tg_user, text):
+    message = Message.objects.create(
+        tg_user=tg_user,
+        text=text,
+        status=SENT
+    )
+
+    return message
+
+
+def create_tg_user(chat_id, psy):
+    tg_user = TelegramUser.objects.create(
+        chat_id=chat_id,
+        main_psy=psy,
+        active_psy=psy
+    )
+    return tg_user
+
+def get_psy(code):
+    psy = get_object_or_404(User,  psy_code=code)
+    return psy
 
 def get_tg_user(chat_id):
     tg_user = get_object_or_404(TelegramUser, chat_id=chat_id)
@@ -14,7 +39,7 @@ def get_or_create_current_state(chat_id):
             state: 0,
         }
     )
-    return chat_state.state
+    return chat_state
 
 
 # def create_tg_user(chat_id):
