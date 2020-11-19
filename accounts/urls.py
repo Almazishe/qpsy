fcfbc2a54f0c27ba008d77bea379598f46af24c3
Almazishe <mr.esenyazov@gmail.com>
2020-11-20 03:01:43 +0600

    fed

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('update-status', views.change_status)
+    path('update-status', views.change_status),
     path('create', views.create_user),
     path('list', views.users_list),
     path('update-email', views.update_email),
